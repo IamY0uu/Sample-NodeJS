0c30972c49b6980a6998e095902c57acb84f5daf
HEMANT SINGH NEGI <168597144+IamY0uu@users.noreply.github.com>
2025-07-05 11:38:18 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-cd /home/ec2-user/sample-app
-npm start &
 
+cd /home/ec2-user/sample-app
+
+# Optionally, install dependencies if not already
+npm install
+
+# Start app in background and log output
+nohup node server.js > app.log 2>&1 &
